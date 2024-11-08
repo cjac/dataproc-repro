b727ef931d7fc8b5c18f1570fa2eb8db197a434f
C.J. Collier <cjac@google.com>
2024-11-08 12:30:38 -0800

    now synchronizing over one million packages

--- lib/conda-mirror/sync-mirror.pl
+++ lib/conda-mirror/sync-mirror.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl
+package CTL78::Conda::Mirror::Sync;
 
 use v5.36;
 
@@ -21,84 +22,91 @@ use File::Spec;
 
 my $mech = WWW::Mechanize->new();
 
-my $repodata = {};
+my( @url, @skipped )=();
 
-#my $response = $mech->get('https://conda.anaconda.org/conda-forge/linux-64/repodata.json');
-if ( ! -e 'conda-forge-linux-64-repodata.json' ){
-  qx(curl -o conda-forge-linux-64-repodata.json https://conda.anaconda.org/conda-forge/linux-64/repodata.json > /dev/null 2>&1)
-}
-my $response = $mech->get('file:conda-forge-linux-64-repodata.json');
-if( $response->is_success ){
-  $repodata->{'linux-64'}  = decode_json $response->decoded_content;
-}
-#$response = $mech->get('https://conda.anaconda.org/conda-forge/noarch/repodata.json');
-if ( ! -e 'conda-forge-noarch-repodata.json' ){
-  qx(curl -o conda-forge-noarch-repodata.json https://conda.anaconda.org/conda-forge/noarch/repodata.json > /dev/null 2>&1)
-}
-$response = $mech->get('file:conda-forge-noarch-repodata.json');
-if( $response->is_success ){
-  $repodata->{'noarch'}  = decode_json $response->decoded_content;
-}
-
-
-my @url=();
-my @skipped=();
-my @not_found=();
-my @malformed=();
-
-foreach my $platform ( qw{ linux-64 noarch } ){
-  while( my( $pkg, $pkgobj ) = each %{$repodata->{"$platform"}->{packages}} ){
-    if( -e "/var/www/html/conda-forge/$pkgobj->{subdir}/$pkg" ){
-#      print('.');
-#      say "file [$pkg] already exists.  skipping";
-      push(@skipped, $pkg);
-    } else {
-      push(@url, "https://conda.anaconda.org/conda-forge/${platform}/$pkg");
+foreach my $platform ( qw( linux-64 noarch ) ){
+  foreach my $channel ( qw(  nvidia rapidsai r main conda-forge  ) ){
+#foreach my $platform ( qw( linux-64 ) ){
+#  foreach my $channel ( qw( main ) ){
+    qx(mkdir -p /var/www/html/${channel}/${platform});
+    my $cache_file = "/tmp/${channel}-${platform}-repodata.json";
+    if ( -e  $cache_file ){
+      $mech->get( "file:$cache_file" );
+    }else{
+      $mech->get( "https://conda.anaconda.org/${channel}/${platform}/repodata.json");
+      $mech->save_content( $cache_file );
     }
-  }
-}
+    my $repodata = decode_json $mech->response()->decoded_content;
+    my @this_url;
+    my @this_skipped;
+
+    #    foreach my $pkg ( keys %{$repodata->{packages}}, keys %{$repodata->{'packages.conda'}} ){
+    foreach my $repokey ( qw{ packages packages.conda } ){
+      printf( 'loading %15s data for %21s... ', $repokey, "${channel}/${platform}" );
+      my $pkghash = $repodata->{$repokey};
+#    foreach my $pkghash ( $repodata->{packages}, $repodata->{'packages.conda'} ){
+      while ( my( $pkg, $pkgobj ) = each ( %$pkghash ) ) {
 
 =pod
 
-my $files; # Gathered from the output of the python conda-mirror program
-$response = $mech->get('file:package-list.txt');
-if( $response->is_success ){
-  $files  = decode_json $response->decoded_content;
-}
-foreach my $filename ( @$files ) {
-  my $fileobj = undef;
-  if( exists $repodata->{'linux-64'}->{packages}->{$filename} ){
-    $fileobj = $repodata->{'linux-64'}->{packages}->{$filename};
-  }elsif( exists $repodata->{'noarch'}->{packages}->{$filename} ){
-    $fileobj = $repodata->{'noarch'}->{packages}->{$filename};
-  }else{
-    if( -e "/var/www/html/conda-forge/$fileobj->{subdir}/$filename" ){
-      say "file [$filename] already exists.  skipping";
-      next;
-    }
-    push(@url, "https://conda.anaconda.org/conda-forge/linux-64/$filename");
-    push(@url, "https://conda.anaconda.org/conda-forge/noarch/$filename");
-    next;
-  }
-  if( -e "/var/www/html/conda-forge/$fileobj->{subdir}/$filename" ){
-    say "file [$filename] already exists.  skipping";
-  }else{
-    push(@url, "https://conda.anaconda.org/conda-forge/$fileobj->{subdir}/$filename");
-  }
-}
+      die Data::Dumper::Dumper($pkgobj);
+
+$VAR1 = {
+          'timestamp' => '1584021296829',
+          'build_number' => 0,
+          'requires' => [],
+          'size' => 1169424,
+          'subdir' => 'linux-64',
+          'license' => 'BSD-3-Clause',
+          'platform' => 'linux',
+          'md5' => '30840c82232dbf1c1c38582ff635980e',
+          'depends' => [
+                         'backcall',
+                         'decorator',
+                         'jedi >=0.10,<0.18',
+                         'pexpect',
+                         'pickleshare',
+                         'prompt_toolkit >=2.0.0,<4,!=3.0.0,!=3.0.1',
+                         'pygments',
+                         'python >=3.7,<3.8.0a0',
+                         'setuptools >=18.5',
+                         'traitlets >=4.2'
+                       ],
+          'sha256' => 'cf3c42ac3b6b9b07bcdb07166c1200a435e9a7f56d7024e2cc35cec4b461b177',
+          'license_family' => 'BSD',
+          'version' => '7.13.0',
+          'source_url' => 'http://repo.continuum.io/pkgs/main/linux-64/ipython-7.13.0-py37h5ca1d4c_0.tar.bz2',
+          'binstar' => {
+                         'package_id' => '5df1f4ff7870580d1c9ead3a',
+                         'owner_id' => '59b17b630c9b040aed6464a8',
+                         'channel' => 'main'
+                       },
+          'arch' => 'x86_64',
+          'name' => 'ipython',
+          'build' => 'py37h5ca1d4c_0'
+        };
 
 =cut
 
-#say Data::Dumper::Dumper( { malformed =>  \@malformed, not_found => \@not_found } );
-#print($/);
+	# Do some filtering here, optionally
+	# my $epoch2017 = 1514764799000;
+	# next if( $pkgobj->{timestamp} < $epoch2017 );
+	if ( -e "/var/www/html/${channel}/${platform}/${pkg}" ) {
+	  push(@this_skipped, $pkg);
+	} else {
+	  push(@this_url, "https://conda.anaconda.org/${channel}/${platform}/$pkg");
+	}
+      }
 
-say sprintf('There have been %i packages fetched', scalar(@skipped));
-say sprintf('there are %i packages left to fetch', scalar(@url));
+      say( printf('%6i skipped, %6i to fetch', scalar( @this_skipped ), scalar(@this_url) ) );
+      push(@skipped, @this_skipped);
+      push(@url, @this_url);
+    }
+  }
+}
 
-say "press enter to continue";
-my $hold=<STDIN>;
-
-my $sem = Coro::Semaphore->new(16);
+my $num_coroutines=8;
+my $sem = Coro::Semaphore->new($num_coroutines);
 
 sub start_thread($){
   my $url = shift(@_);
@@ -110,6 +118,10 @@ sub start_thread($){
     my( $tmp_file,            $output_file,               $guard ) =
       ( "/tmp/$tmp_filename", "/var/www/html/$path_info", $sem->guard );
     my $response = $ua->get( $url );
+    my $tries = 0;
+    until( $ua->response()->is_success || $tries++ > 5){
+      $ua->get( $url );
+    }
     if ( $response->is_success ) {
       $ua->save_content( $tmp_file );
       # move from temp to final directory - possible failure situation
@@ -117,7 +129,10 @@ sub start_thread($){
       # rename output file from temporary name - unlikely to cause failure
       move("$output_file.tmp",$output_file) or die "Copy failed [$output_file.tmp] -> [$output_file]: $!";
       my($l)=length $ua->content;
-      say sprintf( "Fetched %64s (%12d bytes/%5.2f MB) to %90s", $tmp_filename, $l, $l/1024/1024, $output_file );
+      print('.');
+#      say sprintf( "Fetched %64s (%12d bytes/%09.2f MB) to %90s", $tmp_filename, $l, $l/1024/1024, $output_file );
+    }else{
+      say sprintf( "Failed to fetch %64s to %90s", $url, $output_file );
     }
     $ua->delete();
   };
@@ -125,12 +140,15 @@ sub start_thread($){
 
 
 my $completed = 0;
-my($left) = scalar(@url);
 my $nproc = qx(nproc);
-my($buffer_size) = POSIX::ceil($left / $nproc);
+my($left) = scalar(@url);
+my($buffer_size) = POSIX::ceil($left / ($nproc*0.6));
 my($length) = $left < $buffer_size ? $left : $buffer_size;
 
+say('-'x80, $/, "total: ", scalar( @skipped ), " skipped, $left to fetch");
+
 if( $left > 1000 ){
+  say "running as $num_coroutines coroutines in ", POSIX::ceil($nproc*0.6) ," new forked processes";
   my(@children) = ();
   while ( @url ) {
     ($length) = $left < $buffer_size ? $left : $buffer_size;
@@ -138,6 +156,7 @@ if( $left > 1000 ){
     my $pid = fork();
     die "unable to fork: $!" unless defined($pid);
     if (!$pid) {		# child
+#      say "processing batch of ", scalar @batch, " in process $$";
       start_thread $_ for @batch;
       EV::loop;
       exit 0;
@@ -149,7 +168,9 @@ if( $left > 1000 ){
   foreach my $child ( @children ) {
     waitpid $child, 0;
   }
+  say "all children waited for"
 } else {
+  say "running as $num_coroutines coroutines";
   start_thread $_ for @url;
   EV::loop;
 }
